1ee3723bc4b8f5d66ffdd5bb52f549e7691b13e0
CH <ch@albach.com>
2013-06-02 20:09:06 -0700

    Mongoose boilerplate.

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-
-/**
+/*
  * Module dependencies.
  */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express');
+var routes = require('./routes');
+var models = require('./models');
+var http = require('http');
+var path = require('path');
+var mongoose = require('mongoose');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
+// All environments.
+app.set('port', process.env.PORT || 8048);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -23,13 +22,20 @@ app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// For Heroku, use `heroku addons:add mongolab`
+mongoose.connect(process.env.MONGOLAB_URI ||
+                 process.env.MONGOHQ_URL || 
+                 'mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', models.initialize);
+
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ models.js
@@ -0,0 +1,5 @@
+var mongoose = require('mongoose');
+
+exports.initialize = function() {
+  // Register all models here.
+};
